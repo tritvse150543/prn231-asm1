13a81fd8e5cb3b31539ae3a649b0856f5b49b8a0
No One <tqdatqn01230@gmail.com>
2023-09-19 00:04:21 +0700

    first commit

---
+++ BusinessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+        public TEntity FindById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+        public List<TEntity> FindAll(Expression<Func<TEntity, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = _dbSet;
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+            foreach (var property in includeProperties)
+            {
+                query = query.Include(property);
+            }
+            return query.ToList();
+        }
+        public void Add(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            TEntity entity = _dbSet.Find(id);
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ BusinessLayer/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class OrderRepository : GenericRepository<Order>
+    {
+        public OrderRepository(DbContext context) : base(context)
+        {
+        }
+    }
+    public class OrderDetailRepository : GenericRepository<OrderDetail>
+    {
+        public OrderDetailRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
