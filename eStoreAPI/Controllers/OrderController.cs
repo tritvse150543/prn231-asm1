adf15dbbaca17dcd6297e1a31cdbd972ab00784a
trid <dat.trinhquoc@elca.vn>
2023-09-20 21:47:23 +0700

    order-1

---
+++ eStoreAPI/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using DataAccessLayer.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eStoreAPI.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly OrderRepository orderRepository;
+        private readonly OrderDetailRepository detailRepository;
+        private readonly IMapper mapper;
+        public OrderController(OrderRepository orderRepository, OrderDetailRepository detailRepository, IMapper mapper)
+        {
+            this.orderRepository = orderRepository;
+            this.detailRepository = detailRepository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            var orders = orderRepository.FindAll();
+        }
+    }
+}
---
+++ eStoreAPI/DTOs/Order/OrderResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace eStoreAPI.DTOs.Order
+{
+    public class OrderResponseDTO
+    {
+        public int OrderId { get; set; }
+        public int? MemberId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public decimal? Freight { get; set; }
+        public String MemberName { get; set; }
+    }
+}
